88d96cea38c3d20feb70204802ad0a8942a97d6e
MelBIP <s22005@gsm.hs.kr>
2024-04-20 20:19:51 +0900

    first commit

---
+++ App_Jam/BaseVC.swift
@@ -0,0 +1,40 @@
+import UIKit
+import SnapKit
+import Then
+
+public class BaseVC: UIViewController {
+
+    let bounds = UIScreen.main.bounds
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupKeyboardEvent()
+        configureUI()
+        addView()
+        setLayout()
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) { }
+    
+    @objc func keyboardWillHide(_ sender: Notification) { }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func setupKeyboardEvent() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    func configureUI() {}
+    func addView() {}
+    func setLayout() {}
+}
---
+++ App_Jam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App_Jam
+//
+//  Created by 김주은 on 4/20/24.
+//
+
+import UIKit
+
+class ViewController: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
