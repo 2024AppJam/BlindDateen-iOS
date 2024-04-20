fa2522daa0bab964c4d8106db96ee02551c5854e
MelBIP <s22005@gsm.hs.kr>
2024-04-20 23:59:58 +0900

    💄 :: 추천 장소 / 놀거리Card

---
+++ App_Jam/VC/ChatingVC.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ChatingVC: BaseVC {
+    // MARK: Propertices
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func addView() {
+        
+    }
+    
+    override func setLayout() {
+        
+    }
+}
