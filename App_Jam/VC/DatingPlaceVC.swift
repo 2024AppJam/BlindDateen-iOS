dc619add0ea51f46f76c4867b2545c65ea3cf369
MelBIP <s22005@gsm.hs.kr>
2024-04-20 22:44:43 +0900

    💄 :: 데이트장소추천 페이지 세팅

---
+++ App_Jam/VC/DatingPlaceVC.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class DatingPlaceVC: BaseVC {
+    // MARK: Propertices
+    
+    // MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: AddView
+    override func addView() {
+        
+    }
+    
+    // MARK: Setlayout
+    override func setLayout() {
+        
+    }
+}
